02480a97573d9b351cbd47b08765da45fd5ea64c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-01 20:32:33 -0400

    Fix issue with v-active and re-order code
    v-active is now removed if the same btn is clicked again.
    Added comments and cleaned up code.

--- src/js/modules/vdropdown.js
+++ src/js/modules/vdropdown.js
@@ -4,7 +4,9 @@ let listenerEvent = null;
 function activateDropdown() {
     vDropDownMenu.forEach(menu => {
         const dropdownItems = menu.querySelector(".v-dropdown-items");
+        const menuBtn = menu.querySelector(".v-dropdown-obj");
 
+        // Define the type of eventListener based on class
         if (menu.classList.contains("v-click")) {
             listenerEvent = "click";
         } else if (menu.classList.contains("v-hover")) {
@@ -13,30 +15,39 @@ function activateDropdown() {
 
         menu.addEventListener(listenerEvent, (event) => {
             event.stopPropagation();
+
+            // Close all other dropdowns
             vDropDownMenu.forEach(otherMenu => {
                 const otherDropdownItems = otherMenu.querySelector(".v-dropdown-items");
-                const otherDropDownBtns = otherMenu.querySelector(".v-dropdown-obj");
+                const otherMenuBtns = otherMenu.querySelector(".v-dropdown-obj");
                 if (otherDropdownItems !== dropdownItems) {
                     otherDropdownItems.classList.remove("v-visible");
                 }
-                if (otherDropDownBtns !== dropdownItems) {
-                    otherDropDownBtns.classList.remove("v-active");
+                if (otherMenuBtns !== menuBtn) {
+                    otherMenuBtns.classList.remove("v-active");
                 }
             });
-            dropdownItems.classList.toggle("v-visible");
-            const menuBtn = menu.querySelector(".v-dropdown-obj");
-            menuBtn.classList.toggle("v-active");
+
+            // Toggle visibility and active state of the clicked dropdown
+            if (dropdownItems.classList.contains("v-visible")) {
+                dropdownItems.classList.remove("v-visible");
+                menuBtn.classList.remove("v-active");
+            } else {
+                dropdownItems.classList.add("v-visible");
+                menuBtn.classList.add("v-active");
+            }
         });
 
+        // Handle mouseleave to close the dropdown if using mouseenter
         if (listenerEvent === "mouseenter") {
             menu.addEventListener("mouseleave", () => {
                 dropdownItems.classList.remove("v-visible");
-                const menuBtn = menu.querySelector(".v-dropdown-obj");
                 menuBtn.classList.remove("v-active");
             });
         }
     });
 
+    // Close all dropdowns when clicking outside
     document.addEventListener(listenerEvent, () => {
         vDropDownMenu.forEach(menu => {
             const dropdownItems = menu.querySelector(".v-dropdown-items");
